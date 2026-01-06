055c42a4d679290704a0ed9d956a78fb944f2bb1
YaeMonilc <monilc@outlook.com>
2026-01-07 03:33:27 +0800

    Feat: 覆盖kotlin-reflect版本

--- NapCat/build.gradle.kts
+++ NapCat/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
+    api("org.jetbrains.kotlin:kotlin-reflect:2.3.0")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     // https://mvnrepository.com/artifact/io.ktor/ktor-client-core
@@ -18,7 +20,9 @@ dependencies {
     // https://mvnrepository.com/artifact/io.ktor/ktor-client-okhttp-jvm
     api("io.ktor:ktor-client-okhttp-jvm:3.3.3")
     // https://mvnrepository.com/artifact/io.ktor/ktor-client-websocket
-    api("io.ktor:ktor-client-websocket:1.1.4")
+    api("io.ktor:ktor-client-websocket:1.1.4") {
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-reflect")
+    }
     // https://mvnrepository.com/artifact/io.ktor/ktor-serialization-kotlinx-json
     api("io.ktor:ktor-serialization-kotlinx-json:3.3.3")
 
