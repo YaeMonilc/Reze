daee57e97d7e679585de7294eae54aeae9d22424
YaeMonilc <monilc@outlook.com>
2026-01-07 01:16:40 +0800

    Feat: 改善Plugin获取方式

--- Core/src/main/kotlin/io/github/yaemonilc/reze/core/Plugin.kt
+++ Core/src/main/kotlin/io/github/yaemonilc/reze/core/Plugin.kt
@@ -1,10 +1,25 @@
 package io.github.yaemonilc.reze.core
 
+import io.github.yaemonilc.reze.core.entity.RegisteredPlugin
+import java.io.File
+
 interface IPlugin {
     suspend fun onLoaded()
 }
 
-inline fun <reified T : IPlugin> getPluginInstance(): T =
-    reze().pluginManager.byClazz(
-        clazz = T::class
-    )?.instance as? T ?: throw NullPointerException("Not found instance of ${T::class.simpleName}")
+object Plugin {
+    @PublishedApi
+    internal inline fun <reified T : IPlugin> registeredInstance(): RegisteredPlugin? =
+        reze().pluginManager.byClazz(
+            clazz = T::class
+        )
+
+    inline fun <reified T : IPlugin> instance(): T =
+        registeredInstance<T>()?.instance as? T
+            ?: throw NullPointerException("Not found instance of ${T::class.simpleName}")
+
+    inline fun <reified T : IPlugin> privateDirectory(): File =
+        registeredInstance<T>()?.privateDirectory
+            ?: throw NullPointerException("Not found instance of ${T::class.simpleName}")
+
+}
